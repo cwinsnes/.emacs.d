f1fc57856043726c5ba5e33dfc8b2ff0bffa9ee1
Casper Winsnes <cwinsnes92@gmail.com>
2020-01-15 17:04:40 +0100

    Add project management packages

--- init.el
+++ init.el
@@ -9,3 +9,17 @@
 (package-install 'use-package))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/emacsinit.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (counsel-etags emacs-lisp eyebrowse dashboard doom-themes ivy-posframe ivy-rich counsel ivy evil-magit evil-org evil use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
